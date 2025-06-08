bf257b23d0cc38214f006a52f79ca5624ec560cd
Rei <nakatani@qunasys.com>
2025-06-08 17:04:21 +0900

    motion losation weather 追加

--- src/remo/light.js
+++ src/remo/light.js
@@ -11,3 +11,16 @@ function LightSet() {
 
     UrlFetchApp.fetch(url, options);
 }
+
+/*
+LightSetは一回呼び出せば前の状態を反転させる(オン→オフ,オフ→オン).
+これだと状態の判別が出来ないからオンとオフそれぞれ専用の関数を作りたい.
+自分の家のリモコンにはトリガー型のボタンしか無く、それぞれの赤外線idが得られないので、オンオフそれぞれのボタンがあるリモコンを持ってる人にお願いしたい.
+
+function LightOn()       //ライトをつける関数
+
+function LightOff()      //ライトを消す関数
+
+function LightState()    //ライトの状態取得する関数
+
+*/
