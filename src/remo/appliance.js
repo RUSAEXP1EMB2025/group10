c49742af0d0913d02ab03f67fed580d54a41c17c
Rei Nakatani <nakatani@qunasys.com>
2025-06-25 15:42:44 +0900

    最終反応時刻取得

--- src/remo/appliance.js
+++ src/remo/appliance.js
@@ -16,3 +16,26 @@ function getNatureRemoData(endpoint) {
     // データはJSON形式で取得できるため、それをJSON.parseでオブジェクトにして、それを返り値とする.
     return JSON.parse(UrlFetchApp.fetch("https://api.nature.global/1/" + endpoint, options));
 }
+
+function getLastTime() {
+  const deviceData = getNatureRemoData("devices"); // data取得
+
+  const motion = deviceData[0].newest_events.mo;
+
+  return motion.created_at; // 文字列 "2025-06-25T04:12:02Z" (例)を返す
+}
+
+function timeConversion() {
+  const world_time = getLastTime();
+  const utcTime = new Date(world_time); // 日本時間に変換
+
+  // 時:分:秒だけを取り出す
+  const timeString = utcTime.toLocaleTimeString("ja-JP", {
+    hour12: false,             // 24時間制
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit"
+  });
+
+  return timeString;
+}
