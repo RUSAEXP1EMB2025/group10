a4eea4489dfae89923293e73034ca7014446a1f3
Rei Nakatani <nakatani@qunasys.com>
2025-06-30 12:12:16 +0900

    add state of light

--- src/remo/light.js
+++ src/remo/light.js
@@ -9,6 +9,8 @@ function LightOn() {
         }
     };
 
+    PropertiesService.getScriptProperties().setProperty("LIGHT_STATE", "1");
+
     UrlFetchApp.fetch(url, options);
 }
 
@@ -23,27 +25,7 @@ function LightOff() {
         }
     };
 
+    PropertiesService.getScriptProperties().setProperty("LIGHT_STATE", "0");
+
     UrlFetchApp.fetch(url, options);
-}
-
-function LightState() {
-    const devices = getNatureRemoData("devices");   //登録している家具のデータがJSON形式でdevicesに入る
-
-    /*
-    result = Array.find( func() ) では配列Arrayの要素を頭から走査していって,各要素に対して,引数のfuncを呼び出す.そして、最初にtrueになった要素をresultに代入する.
-
-    アロー演算子=>は、関数である.
-    device => device.type === "LIGHT"を書き換えると以下のようになる.
-    
-    function func(device) {
-        return device.type === "LIGHT";
-    }
-
-    つまり以下の一行はdevice.typeが"LIGHT"の要素を探していて、最初に見つかったのをdevideに代入している.
-    */
-    const find_device = devices.find(device => device.type === "LIGHT");
-
-    const state = find_device.light.state;  //"state"の項目をstateに代入
-
-    return state.power; // "on" または "off"を返す
 }
